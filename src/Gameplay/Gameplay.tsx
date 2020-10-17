57ae801d4f764c04da8cce8766a93b3f76e829ee
Leigh Larson <leighlarson4@gmail.com>
2020-10-17 04:28:43 -0600

    Add Gameplay

--- src/Gameplay/Gameplay.tsx
+++ src/Gameplay/Gameplay.tsx
@@ -19,7 +19,7 @@ export const Gameplay: React.FC<GameplayProps> = props => {
       <p>- Best if played with friends for optimal interview feedback</p>
       <p>- Enter candidate's name to keep track of questions asked</p>
       <p>- One person is interviewer, one person is candidate</p>
-      <p>- Other players may privately message interviewer their thoughts on candidate to write in comment box</p>
+      <p>- Other players may privately message interviewer their thoughts on candidate to write in comment box and not distract candidate</p>
       <p>- Each round has 5 questions</p>
       <p>- Interviewer will add notes once interview is complete, click 'add', and notes will render to discuss with candidate</p>
       <p>- At the end of the round, the interviewer will be alerted to compile group comments and submit in form</p>
